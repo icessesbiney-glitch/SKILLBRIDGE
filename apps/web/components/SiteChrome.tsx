de9c23fc1da8f2088637b562b449fa24ca77a5c7
icessesbiney-glitch <icesses.biney@gmail.com>
2026-09-23 22:07:49 +0000

    Update SiteChrome.tsx

--- apps/web/components/SiteChrome.tsx
+++ apps/web/components/SiteChrome.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 import AuthControls from './AuthControls';
 import { navigationLinks } from '../data/siteContent';
-import { supabase } from '@/utils/supabaseClient';
+import { supabase } from '../utils/supabaseClient';
 
 type SiteChromeProps = {
   children: React.ReactNode;
