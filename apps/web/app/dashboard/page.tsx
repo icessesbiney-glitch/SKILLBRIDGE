03d7c50d9b96bfb5bb59a95f37901172de748b1f
icessesbiney-glitch <icesses.biney@gmail.com>
2026-09-24 03:00:54 +0000

    Update page.tsx

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link';
+'use client';
 
+import Link from 'next/link';
 import AuthStatusCard from '../../components/AuthStatusCard';
 import ProtectedShell from '../../components/ProtectedShell';
 import SiteChrome from '../../components/SiteChrome';
